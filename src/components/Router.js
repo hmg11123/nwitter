f4f05ef8c666030c135d4a73f095ab70133dac16
hmg11123 <you@example.com>
2021-02-09 11:33:13 +0900

    1.2

---
+++ src/components/Router.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Auth from "../router/Auth";
+import Home from "../router/Home";
+import EditProfile from "../router/EditProfile";
+import Profile from "../router/Profile";
+
+const AppRouter = () => {
+ const [isLoggedIn, setIsLoggedIn] = useState(false);
+ return (
+  <Router>
+   <Switch>
+    {isLoggedIn ? (
+     <Route exact path="/">
+      <Home />
+     </Route>
+    ) : (
+     <Route exact path="/">
+      <Auth />
+     </Route>
+    )}
+   </Switch>
+  </Router>
+ );
+};
+
+export default AppRouter;
